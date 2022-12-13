2878b89ac2008a3dbc5dd3380f101527e0998f86
yulongbb <sunyulongbb@gmail.com>
2022-12-13 14:31:21 +0800

    引入依赖

--- apps/graph/src/main.tsx
+++ apps/graph/src/main.tsx
@@ -1,3 +1,4 @@
+import '@lib/shared';
 import React from "react";
 import ReactDOM from "react-dom";
 
--- apps/graph/src/views/Root.tsx
+++ apps/graph/src/views/Root.tsx
@@ -1,24 +1,29 @@
-import React, { FC, useEffect, useState } from "react";
-import { SigmaContainer, ZoomControl, FullScreenControl } from "react-sigma-v2";
-import { omit, mapValues, keyBy, constant } from "lodash";
+import React, { FC, useEffect, useState } from 'react';
+import { SigmaContainer, ZoomControl, FullScreenControl } from 'react-sigma-v2';
+import { omit, mapValues, keyBy, constant } from 'lodash';
 
-import getNodeProgramImage from "sigma/rendering/webgl/programs/node.image";
+import getNodeProgramImage from 'sigma/rendering/webgl/programs/node.image';
 
-import GraphSettingsController from "./GraphSettingsController";
-import GraphEventsController from "./GraphEventsController";
-import GraphDataController from "./GraphDataController";
-import DescriptionPanel from "./DescriptionPanel";
-import { Dataset, FiltersState } from "../types";
-import ClustersPanel from "./ClustersPanel";
-import SearchField from "./SearchField";
-import drawLabel from "../canvas-utils";
-import GraphTitle from "./GraphTitle";
-import TagsPanel from "./TagsPanel";
+import GraphSettingsController from './GraphSettingsController';
+import GraphEventsController from './GraphEventsController';
+import GraphDataController from './GraphDataController';
+import DescriptionPanel from './DescriptionPanel';
+import { Dataset, FiltersState } from '../types';
+import ClustersPanel from './ClustersPanel';
+import SearchField from './SearchField';
+import drawLabel from '../canvas-utils';
+import GraphTitle from './GraphTitle';
+import TagsPanel from './TagsPanel';
 
-import "react-sigma-v2/lib/react-sigma-v2.css";
-import { GrClose } from "react-icons/gr";
-import { BiRadioCircleMarked, BiBookContent } from "react-icons/bi";
-import { BsArrowsFullscreen, BsFullscreenExit, BsZoomIn, BsZoomOut } from "react-icons/bs";
+import 'react-sigma-v2/lib/react-sigma-v2.css';
+import { GrClose } from 'react-icons/gr';
+import { BiRadioCircleMarked, BiBookContent } from 'react-icons/bi';
+import {
+  BsArrowsFullscreen,
+  BsFullscreenExit,
+  BsZoomIn,
+  BsZoomOut,
+} from 'react-icons/bs';
 
 const Root: FC = () => {
   const [showContents, setShowContents] = useState(false);
@@ -37,8 +42,8 @@ const Root: FC = () => {
       .then((dataset: Dataset) => {
         setDataset(dataset);
         setFiltersState({
-          clusters: mapValues(keyBy(dataset.clusters, "key"), constant(true)),
-          tags: mapValues(keyBy(dataset.tags, "key"), constant(true)),
+          clusters: mapValues(keyBy(dataset.clusters, 'key'), constant(true)),
+          tags: mapValues(keyBy(dataset.tags, 'key'), constant(true)),
         });
         requestAnimationFrame(() => setDataReady(true));
       });
@@ -47,18 +52,20 @@ const Root: FC = () => {
   if (!dataset) return null;
 
   return (
-    <div id="app-root" className={showContents ? "show-contents" : ""}>
+    <div id="app-root" className={showContents ? 'show-contents' : ''}>
+      <my-greeting title="react"></my-greeting>
+
       <SigmaContainer
-        graphOptions={{ type: "directed" }}
+        graphOptions={{ type: 'directed' }}
         initialSettings={{
           nodeProgramClasses: { image: getNodeProgramImage() },
           labelRenderer: drawLabel,
-          defaultNodeType: "image",
-          defaultEdgeType: "arrow",
+          defaultNodeType: 'image',
+          defaultEdgeType: 'arrow',
           labelDensity: 0.07,
           labelGridCellSize: 60,
           labelRenderedSizeThreshold: 15,
-          labelFont: "Lato, sans-serif",
+          labelFont: 'Lato, sans-serif',
           zIndex: true,
         }}
         className="react-sigma"
@@ -137,7 +144,9 @@ const Root: FC = () => {
                   toggleTag={(tag) => {
                     setFiltersState((filters) => ({
                       ...filters,
-                      tags: filters.tags[tag] ? omit(filters.tags, tag) : { ...filters.tags, [tag]: true },
+                      tags: filters.tags[tag]
+                        ? omit(filters.tags, tag)
+                        : { ...filters.tags, [tag]: true },
                     }));
                   }}
                 />
