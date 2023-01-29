25c93ba88652d1106d694055731251b360c00670
yulongbb <sunyulongbb@gmail.com>
2023-01-29 17:28:35 +0800

    提交

--- tools/data/starbucks/starbucks.py
+++ tools/data/starbucks/starbucks.py
@@ -12,6 +12,8 @@ class StarbucksSpider(scrapy.Spider):
                 yield {'category': category.css('.caption::text').get()}
                 for product in category.css('li>a'):
                     yield {'product': product.css('strong::text').get()}
-                    yield {'preview': 'https://www.starbucks.com.cn/' +product.css('.preview').xpath('@style').re(r'background-image: url(\s)*(.*)')[1].split('"')[1]}
-                    writer.writerow([product.css('strong::text').get(), category.css('.caption::text').get(), "类型"])
-                    writer.writerow([product.css('strong::text').get(), 'https://www.starbucks.com.cn' +product.css('.preview').xpath('@style').re(r'background-image: url(\s)*(.*)')[1].split('"')[1], "图片"])
+                    yield {'preview': 'https://www.starbucks.com.cn/' + product.css('.preview').xpath('@style').re(r'background-image: url(\s)*(.*)')[1].split('"')[1]}
+                    writer.writerow([product.css('strong::text').get(
+                    ), category.css('.caption::text').get(), "类型"])
+                    writer.writerow([product.css('strong::text').get(), 'https://www.starbucks.com.cn' + product.css(
+                        '.preview').xpath('@style').re(r'background-image: url(\s)*(.*)')[1].split('"')[1], "图片"])
